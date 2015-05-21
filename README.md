a2a03dca78d0d36bcaefa50c739e2fea2cd6f1cf
Sean Munoz <Sean@peijobs.com>
2015-05-21 10:45:24 -0700

    Added Java file and updated README all from within Eclipse.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # hello-world
 Yep, it's my first repository
 
-And here's a line that I've added.
+And here's a line that I've added. (and changed this line)
+
+I added this line using Eclipse. 
---
+++ src/com/seanmunoz/first/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.seanmunoz.first;
+
+public class HelloWorld {
+	/*
+	 * Sean's VERY first Java program.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+		System.out.printf("\nGoodbye.\n");
+	}
+
+}
